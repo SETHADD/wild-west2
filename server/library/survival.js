64dcc72ebc2e88ce6362160c9e0997f42057d376
Seth Kwame Addo <addoseth7@gmail.com>
2023-07-27 14:18:03 +0100

    initi

---
+++ server/library/survival.js
@@ -0,0 +1,19 @@
+
+const survival = (request,response)=>{
+    // do something
+    const scenarios = [
+      "You made it to the saloon. Congrats",
+      "You died of dysentery.",
+      "A snake gotcha!",
+      "Your horse made it... you didn't.",
+      "Your made it... your horse didn't.",
+      "You found gold!",
+      "You found oil!",
+      "You didn't even attempt to cross the Oregon trail.",
+    ];
+    const randomNum = Math.floor(Math.random() * scenarios.length);
+    const outcome = scenarios[randomNum];
+    return(response.json(outcome))
+  
+}
+  module.exports = survival();
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+
+const cors = require("cors")
+
+require("dotenv").config()
+
+const PORT = process.env.PORT||8080
+
+const app = express()
+
+app.use(cors())
+
+const survival = require("./library/survival");
+const maths = require("./library/maths")
+
+app.get('/', (request,response)=>response.json("your name is here"))
+
+app.get("/survival",survival)
+app.get("/maths",maths)
+
+
+
+app.listen(PORT,()=>console.log(`we are listening on port ${PORT}`))
