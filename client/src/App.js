b4f6847b4d83e448653cd2a62f49cc229b98e6c6
Seth Kwame Addo <addoseth7@gmail.com>
2023-07-28 07:59:53 +0100

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 
+import { useState } from 'react';
 import './App.css';
+import axios from "axios"
 
 function App() {
+const [handleClick,sethandleClick] = useState('')
+
+const handleAPI = async()=> {
+    const API = `http://localhost:8080/survival`
+    const res = await axios.get(API)
+    sethandleClick(res.data)
+    console.log(res)
+}
+
+
   return (
     <div className="App">
-
+    <button onClick={handleAPI}>CLICK TO CHANGE</button>
+    <p>{handleClick}</p>
     </div>
   );
 }
